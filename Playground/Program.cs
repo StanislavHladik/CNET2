c5f3c34ec110244061f1f0f694ac3546ed6131f5
Stanislav Hladik <Stanislav.Hladik@saint-gobain.com>
2021-11-22 10:54:45 +0100

    tisk listů

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -5,18 +5,19 @@ var numbers = new[] { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
 var strings = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 
 
-var results1 = strings.Select(x => x.ToUpper());
+var results = strings.Select(x => x.ToUpper());
 
-IEnumerable<string> results2 = from s in strings
+/*
+IEnumerable<string> results = from s in strings
                             select s.ToUpper();
+*/
 
+PrintList(results.ToList());
 
-foreach(string r in results1)
+static void PrintList(List<string> listToPrint)
 {
-    Console.WriteLine(r);
-}
-
-foreach (string r in results2)
-{
-    Console.WriteLine(r);
+    foreach (var item in listToPrint)
+    {
+        Console.WriteLine(item);
+    }
 }
